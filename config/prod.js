f44cf79ca6d95316f61fae8c9d1f402db5f1c804
LOUTAG <tagliasco.lou@orange.fr>
2022-06-28 14:56:20 +0200

    setting prod.js

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports={
+    mongoURI: process.env.MONGO_URI,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+    ACCESS_TOKEN_EXP: process.env.ACCESS_TOKEN_EXP,
+    REFRESH_TOKEN: process.env.REFRESH_TOKEN,
+    REFRESH_TOKEN_EXP: process.env.REFRESH_TOKEN_EXP,
+    sendgridApiKey: process.env.SENDGRID_KEY,
+    CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET_KEY: process.env.CLOUDINARY_API_SECRET_KEY
+}
